f9a84e20cd2181159121148848e295a3ffed7a6e
KAUSTAV BHATTACHARYA <kaustav.bhattacharya@codelogicx.com>
2022-06-27 11:52:55 +0530

    fix route method.

--- src/app/index.js
+++ src/app/index.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(bodyparser.json())
 
-app.use("/",(req, res)=> res.status(200).send("Success"))
+app.get("/",(req, res)=> res.status(200).send("success"))
 
 app.use("/api", router)
 
