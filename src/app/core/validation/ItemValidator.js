e71791e8725ae67d2508ca14b07cc93bd97794eb
KAUSTAV BHATTACHARYA <kaustavofficial1808@gmail.com>
2023-03-05 13:17:56 +0530

    Item api create Item.

---
+++ src/app/core/validation/ItemValidator.js
@@ -0,0 +1,24 @@
+const Joi = require('joi')
+const Validator = require('./Validator')
+
+class ItemCreateValidator extends Validator {
+    // eslint-disable-next-line class-methods-use-this
+    getOptions() {
+        return {}
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    getRules() {
+        return {
+            title: Joi.string().required(),
+            shortDescription: Joi.string().required(),
+            description: Joi.string().required(),
+            tags: Joi.string().required(),
+            sellType: Joi.string().valid("range", "auction").required(),
+            minPrice: Joi.number().min(0).required(),
+            maxPrice: Joi.number().required(),
+        }
+    }
+}
+
+module.exports = ItemCreateValidator
--- src/app/routes/item.js
+++ src/app/routes/item.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const passport = require('../utils/passport')
 const ItemController = require('../controllers/item')
+const Validate = require('../core/validation')
+const ItemCreateValidator = require('../core/validation/ItemValidator')
 
 const router = express.Router()
 
 router.get('/', (req, res, done) => {
     ItemController.itemList()
+        .then((result) => res.status(200).json(result))
+        .catch((err) => done(err, null))
+})
+
+router.post('/',
+            passport.authenticate('verify_token', { session: false }),
+            (req, _, next) => Validate(new ItemCreateValidator(), req, next), 
+            (req, res, done) => {
+    ItemController.createItem(req)
         .then((result) => res.status(200).json(result))
         .catch((err) => done(err, null))
 })
